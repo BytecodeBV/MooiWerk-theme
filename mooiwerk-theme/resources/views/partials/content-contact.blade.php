9272e411590bb61cb371277a8f4ad888331ff00e
Ikpugbu George <gaiprojectz@gmail.com>
2018-09-22 02:19:35 +0100

    contact page and global anchor color

--- mooiwerk-theme/resources/views/partials/content-contact.blade.php
+++ mooiwerk-theme/resources/views/partials/content-contact.blade.php
@@ -1,57 +1,59 @@
 <section @php post_class('contact') @endphp>
     <div class="page__body container">
-        <div class="row page__content mt-5">
-            <div class="col-md-6 contact__cards">
-                <div class="col-sm-12 my-4">
-                    <div class="card contatct__card border-0">
-                        <div class="card-body">
-                            <p class="mb-4"><i class="fa fa-phone fa-2x mb-2" aria-hidden="true"></i></p>
-                            <div class="big-text">{!! get_field('phone') !!}</div>
-                        </div>
-                    </div>
+        <div class="row page__content contact__cards mt-5">
+            <div class="col-lg-6 mb-3">
+                <h1 class="mb-3">Contact</h1>
+                <div class="media contact__card mb-3 shadow bg-white rounded">
+                    <a class="media-left" href="#">
+                        <img class="media-object p-2" src="@asset('images/phone.png')" width="120" alt="">
+                    </a>
+                    <div class="media-body px-3 pt-3">
+                        {!! get_field('phone') !!}
+                    </div>                    
                 </div>
-                <div class="col-sm-12 my-4">
-                    <div class="card  contatct__card border-0">
-                        <div class="card-body">
-                            <p class="mb-4"><i class="fa fa-envelope fa-2x mb-2" aria-hidden="true"></i></p>
-                            <div class="big-text">{!! get_field('address') !!} </div>
-                        </div>
-                    </div>
+                <div class="media contact__card mb-3 shadow bg-white rounded">
+                    <a class="media-left" href="#">
+                        <img class="media-object p-2" src="@asset('images/envelope.png')" width="120" alt="">
+                    </a>
+                    <div class="media-body px-3 pt-3">
+                        {!! get_field('address') !!}
+                    </div>                    
                 </div>
-                <div class="col-sm-12 my-4">
-                    <div class="card  contatct__card border-0">
-                        <div class="card-body">
-                            <p class="mb-4"><i class="fa fa-at fa-2x mb-2" aria-hidden="true"></i></p>
-                            <div class="big-text">{{get_field('email')}}</div>
-                        </div>
-                    </div>
+                <div class="media contact__card mb-3 shadow bg-white rounded">
+                    <a class="media-left" href="#">
+                        <img class="media-object p-2" src="@asset('images/at.png')" width="120" alt="">
+                    </a>
+                    <div class="media-body px-3 pt-3">
+                        {{get_field('email')}}
+                    </div>                    
                 </div>
-                <div class="col-sm-12 my-4">
-                    <div class="card  contatct__card border-0">
-                        <div class="card-body">                            
-                            <p class="mb-4"><i class="fa fa-map-marker fa-2x mb-2" aria-hidden="true"></i></p>
-                            <div class="big-text">{!! get_field('info') !!}</div>
-                        </div>
-                    </div>
-                </div>
+                <div class="media contact__card mb-3 shadow bg-white rounded">
+                    <a class="media-left" href="#">
+                        <img class="media-object p-2" src="@asset('images/map-marker.png')" width="120" alt="">
+                    </a>
+                    <div class="media-body px-3 pt-3">
+                        {!! get_field('info') !!}
+                    </div>                    
+                </div>                
             </div>
-            <div class="col-md-6  contact__form">
+            <div class="col-lg-6 mb-3">
                 @php
                     $teams = App::teams();
                 @endphp
                 @if($teams)
+                    <h1 class="mb-3"> TEAM </h1>
                     @foreach($teams as $team)
-                        <div class="d-flex flex-row border rounded mb-4">
-                            <div class="p-0 w-25">
-                                <img src="{{$team['avatar']}}" class="img-thumbnail border-0" />                        
-                            </div>
-                            <div class="pl-3 pt-2 pr-2 pb-2 w-75 border-left">
-                                    <h4 class="text-primary">{{$team['name']}}</h4>
-                                    <ul class="m-0 float-left" style="list-style: none; margin:0; padding: 0">
-                                        <li><i class="fab fa-facebook-square"></i> {{$team['phone']}}</li>
-                                        <li><i class="fab fa-twitter-square"></i> {{$team['email']}}</li>
+                        <div class="media contact__card mb-3 shadow bg-white rounded">
+                            <a class="media-left" href="#">
+                                <img class="media-object" src="{{$team['avatar']}}" width="120" alt="">
+                            </a>
+                            <div class="media-body ml-3 mt-2">
+                                <h4 class="media-heading">{{$team['name']}}</h4>
+                                    <ul class="m-0 contact__list">
+                                        <li class="contact__list-item"><i class="fa fa-phone"></i> {{$team['phone']}}</li>
+                                        <li class="contact__list-item"><i class="fa fa-envelope"></i> {{$team['email']}}</li>
                                     </ul>
-                            </div>
+                            </div>                    
                         </div>
                     @endforeach
                 @endif
